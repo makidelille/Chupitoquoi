20268e443dea6edcac24d2836aec0551a42e3734
Julien Donque <julien@jdonque.fr>
2025-07-09 19:40:15 +0000

    improve render and animation

--- src/lib/Filter.svelte
+++ src/lib/Filter.svelte
@@ -1,52 +1,54 @@
-
 <script>
-    import {  onMount } from "svelte";
+    import { onMount } from "svelte";
 
-    
-    let {
-        available = $bindable(),
-        list
-    } = $props();
+    let { available = $bindable(), list } = $props();
 
-    const tags = $derived(new Set(list.map(s => s.tags).flat()));
-
+    const tags = $derived(new Set(list.map((s) => s.tags).flat()));
 
     onMount(() => {
-        tags.forEach(t => available.push(t));
-    })
-
-    
-
-    $effect(() => {
-        console.log(available); 
-    })
-
-
+        tags.forEach((t) => available.push(t));
+    });
 </script>
 
 {#if tags.size}
+    <h4 class="flex w-full">Types de shot:</h4>
 
-<h4 class="flex w-full">
-    Types de shot:
-</h4>
-
-<ul class="flex flex-row gap-2 flex-wrap w-full">
-    {#each tags as t}
-    <label for={t} class="p-1 bg-gray-100 text-black rounded flex flex-row gap-1 items-center justify-between w-auto capitalize">
-        <input id={t} name={t} type="checkbox" checked={available.includes(t)}
-         onclick={
-            () => {
-                if(available.includes(t)) {
-                    available = available.filter(a => a != t);
-                } else {
-                    available.push(t);
-                }
-            }
-        }/>
-        <span class="text-nowrap">{t}</span>
-    </label>
-    
-    {/each}
-</ul>
-
+    <ul class="flex flex-row gap-2 flex-wrap w-full">
+        {#each tags as t}
+            <label
+                for={t}
+                class="p-1 bg-gray-700 text-white rounded flex flex-row gap-1 items-center justify-between w-auto capitalize relative"
+            >
+                <input
+                    id={t}
+                    name={t}
+                    class="hidden"
+                    type="checkbox"
+                    checked={available.includes(t)}
+                    onclick={() => {
+                        if (available.includes(t)) {
+                            available = available.filter((a) => a != t);
+                        } else {
+                            available.push(t);
+                        }
+                    }}
+                />
+                <span class="text-nowrap">{t}</span>
+                <div class="left-1 right-1 h-0 absolute border"></div>
+            </label>
+        {/each}
+    </ul>
 {/if}
+
+<style>
+    label:has(input:checked) {
+        color: black;
+        background-color: var(--color-gray-100)
+    }
+
+    label:has(input:checked) div {
+        display: none;
+    }
+    
+
+</style>
--- src/lib/Roulette.svelte
+++ src/lib/Roulette.svelte
@@ -1,40 +1,39 @@
 <script>
-    import { onDestroy, onMount } from "svelte";
-    import { quintInOut } from "svelte/easing";
-    import Filter from "./Filter.svelte";
-    import Marquee from "./Marquee.svelte";
-    import Shot from "./Shot.svelte";
+  import { onDestroy, onMount } from "svelte";
+  import { quintInOut } from "svelte/easing";
+  import Filter from "./Filter.svelte";
+  import Marquee from "./Marquee.svelte";
+  import Shot from "./Shot.svelte";
 
-
-
-let {shots} = $props();
+  let { shots } = $props();
 
   let interval;
-  const globalDuration = 3000;
+  const globalDuration = 5000;
   const tickRate = 1000 / 60;
-
 
   let width1 = $state();
   let widthmax = $state();
-  let offsetIndex = $derived(widthmax / width1 * 0.5)
-
+  let offsetIndex = $derived((widthmax / width1) * 0.5);
 
   let randomlist = $derived(
-    shots.filter(s => 
-    s.tags.length === 0 || s.tags.every(tag => available.includes(tag))
-  ).sort(() => Math.random() - 0.5)
-);
+    shots
+      .filter(
+        (s) =>
+          (!exclusive && s.tags.length === 0) ||
+          (s.tags.length && s.tags.every((tag) => available.includes(tag))),
+      )
+      .sort(() => Math.random() - 0.5),
+  );
 
-
-  let scrollPos  = $state(0);
+  let scrollPos = $state(0);
   let running = $state(false);
   let done = $state(false);
 
+  let exclusive = $state(false);
+
   let available = $state([]);
-  
+
   let winner = $state(undefined);
-
-
 
   function startAnimation() {
     running = true;
@@ -42,33 +41,41 @@ let {shots} = $props();
     clearInterval(interval);
     interval = setInterval(() => {
       progress += (tickRate / globalDuration) * Math.random();
-      if(progress >= 1)  {
-        clearInterval(interval);
-        done = true;
-        let index = (scrollPos) / width1;
-        winner = randomlist[~~(index + offsetIndex) % randomlist.length];
-        return;
+      if (progress >= 1) {
+        let index = scrollPos / width1;
+        const winnerIndex = ~~(index + offsetIndex) % randomlist.length;
+        let target = (winnerIndex - (offsetIndex)) * width1  + width1 / 2;
+        const delta = Math.abs(scrollPos - target);
+
+        if(delta < 10) {
+          clearInterval(interval);
+          done = true;
+          winner = randomlist[winnerIndex];
+          return;
+        } else {
+          scrollPos -= (scrollPos - target)/Math.abs((scrollPos - target)) * 2.5;
+          return;
+        }
+
       }
 
-      scrollPos += (1 - quintInOut(progress)) * 1000;
+      scrollPos += Math.abs(((1 - quintInOut(progress)))) * 1000;
     }, tickRate);
   }
 
   onMount(() => {
     reset();
-  })
+  });
 
   onDestroy(() => {
-    clearInterval(interval)
-    });
-    
+    clearInterval(interval);
+  });
 
   function autoScroll() {
     interval = setInterval(() => {
-      scrollPos += 2.5
+      scrollPos += 2.5;
     }, tickRate);
   }
-
 
   function reset() {
     done = false;
@@ -77,50 +84,66 @@ let {shots} = $props();
     autoScroll();
   }
 
-    function scrollIntoView(node) {
-        node.scrollIntoView({
-          behavior: "smooth"
-        })
-    }
+  function scrollIntoView(node) {
+    node.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
 </script>
-  
-  
-  <Filter bind:available={available} list={shots}></Filter>
- 
-  <section class="text-white rounded relative w-full  h-48">
-    <div class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center py-4" bind:clientWidth={widthmax}>
-      <Marquee bind:scrollLeft={scrollPos}  >
-        {#each randomlist as s, i}
+
+<label
+  class="self-start flex gap-2 text-start p-1 bg-gray-100 text-black rounded"
+>
+  Exclure les "bases"
+  <input type="checkbox" id="exclusive" bind:checked={exclusive} />
+</label>
+
+<Filter bind:available list={shots}></Filter>
+
+<section class="text-white rounded relative w-full h-54">
+  <div
+    class="absolute left-0 right-0 h-full overflow-y-hidden flex items-center justify-center p-4"
+    bind:clientWidth={widthmax}
+  >
+    <Marquee bind:scrollLeft={scrollPos}>
+      {#each randomlist as s, i}
         <div class="p-1 w-42 h-full" bind:clientWidth={width1}>
           <Shot shot={s}></Shot>
         </div>
-        {/each}
-      </Marquee>
-    </div>
-    <div class="absolute h-full z-10 top-0 left-0 right-0 bottom-0 flex items-center justify-between">
-      <div class="bg-gradient-to-r from-[var(--background)] to-transparent h-full w-24"></div>
-      <div class="bg-red-500 w-1 h-full"></div>
-      <div class="bg-gradient-to-l from-[var(--background)] to-transparent h-full w-24"></div>
-    </div>
-  </section>
-
+      {/each}
+    </Marquee>
+  </div>
+  <div
+    class="absolute h-full z-10 w-full top-0 left-0 right-0 bottom-0 flex items-center justify-between"
+  >
+    <div
+      class="bg-gradient-to-r from-[var(--background)] to-transparent h-full w-24"
+    ></div>
+    <div class="bg-red-500 w-1 h-full"></div>
+    <div
+      class="bg-gradient-to-l from-[var(--background)] to-transparent h-full w-24"
+    ></div>
+  </div>
+</section>
 
 {#if winner}
   <div class=" text-white mt-4 size-64" use:scrollIntoView>
-    <Shot shot={winner}> 
-    </Shot>
+    <Shot shot={winner}></Shot>
   </div>
-     <button  onclick={reset}
-      class="flex bg-white text-blue-500 py-2 px-4 rounded transition-colors duration-300 hover:bg-blue-200"
-  style="font-size: 1.25rem; font-weight: bold;"> 
-  J'en veux un autre&nbsp;💀 </button>
+  <button
+    onclick={reset}
+    class="flex bg-white text-blue-500 py-2 px-4 rounded transition-colors duration-300 hover:bg-blue-200"
+    style="font-size: 1.25rem; font-weight: bold;"
+  >
+    J'en veux un autre&nbsp;💀
+  </button>
 {:else if !running}
-  
-<button class="bg-white text-blue-500 px-4 py-2 rounded transition-colors duration-300 hover:bg-blue-200"
-  style="font-size: 1.5rem; font-weight: bold;"
-disabled={running} onclick={startAnimation}>
-  Trouve moi un shot&nbsp;🔥
-</button>
-
-{:else}
-{/if}
+  <button
+    class="bg-white text-blue-500 px-4 py-2 rounded transition-colors duration-300 hover:bg-blue-200"
+    style="font-size: 1.5rem; font-weight: bold;"
+    disabled={running}
+    onclick={startAnimation}
+  >
+    Trouve moi un shot&nbsp;🔥
+  </button>
+{:else}{/if}
